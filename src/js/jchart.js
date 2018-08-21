10c1da28b7085867c6255ca1479b3b3e5d31bd4b
Adam Kocić <adam.kocic@hotmail.com>
2018-08-21 13:18:32 +0200

    bugfix - tooltip percentage

--- src/js/jchart.js
+++ src/js/jchart.js
@@ -622,7 +622,7 @@
                                 } else if(instance.settings.appearance.title.showValue) {
                                     title += `: ${titlePartials.segmentValue}`;
                                 } else if(instance.settings.appearance.title.showPercentage) {
-                                    title += `: ${titlePartials.segmentPercentage}`;
+                                    title += `: ${titlePartials.segmentPercentage}%`;
                                 }
                                 //
 
